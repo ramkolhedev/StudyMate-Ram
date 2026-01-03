c1f0fd5687c2d27cac78003ae243af7ffee74b5c
Ram Kolhe <ramkolhe.dev@gmail.com>
2026-01-03 19:21:19 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-
+import logo from "../assets/logo.png";
 function Header() {
 
 
@@ -8,7 +8,7 @@ function Header() {
         <div>
             <header className="flex items-center justify-between px-6 py-4 bg-gray-950 shadow-md shadow-gray-400/10 fixed top-0 left-0 right-0 z-40 border-b border-gray-400/30">
                 <div className="flex items-center gap-3">
-                    <img src="\src\assets\logo.png" style={{borderRadius:'50%'}}   width={'50px'} alt="" />
+                    <img src={logo} style={{borderRadius:'50%'}}   width={'50px'} alt="" />
 
                     <Link to="/" className="text-xl font-semibold text-sky-400">
                         StudyMate
